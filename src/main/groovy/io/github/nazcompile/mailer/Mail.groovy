7781c9b52da394a3134c968271ea8f8d501148ff
Naz <admin@admins-MacBook-Pro.local>
2017-01-21 11:46:10 +0000

    Added functionality to getSession instance

--- src/main/groovy/io/github/nazcompile/mailer/Mail.groovy
+++ src/main/groovy/io/github/nazcompile/mailer/Mail.groovy
@@ -1,30 +1,37 @@
 package io.github.nazcompile.mailer
 
+import javax.mail.Session
+
 class Mail {
-	
+
 	public static def SMTP_HOST
 	public static def SMTP_PORT
 	private static def TRANSPORT_TYPE = 'smtp'
-	
+
 	private static Properties PROPERTIES
-	
+
 	String from = ''
 	List<String> to = []
 	List<String> cc = []
 	List<String> bcc = []
-	List<String> attachments =[]
+	List<String> attachments = []
 	String subject = ''
 	String messageContent = ''
 	String messageType = ''
-	
-	
+
+
 	public static def configure(Closure closure) {
 		closure(this)
-		
+
 		PROPERTIES = new Properties()
 		PROPERTIES.put('mail.smtp.host', SMTP_HOST)
 		PROPERTIES.put('mail.smtp.port', SMTP_PORT)
 		PROPERTIES.put('mail.transport.protocol', TRANSPORT_TYPE)
-	}	
-	
+	}
+
+	private def getSession(Properties props) {
+		def session = Session.getDefaultInstance(props)
+		session
+	}
+
 }
--- src/test/groovy/io/github/nazcompile/mailer/MailSpec.groovy
+++ src/test/groovy/io/github/nazcompile/mailer/MailSpec.groovy
@@ -1,8 +1,16 @@
 package io.github.nazcompile.mailer;
 
+import javax.mail.Session;
+
 import spock.lang.Specification
 
 public class MailSpec extends Specification {
+	
+	Mail mail
+	
+	def setup() {
+		mail = new Mail()
+	}
 	
 	def "Should be able to configure Mail"() {
 		given:
@@ -14,6 +22,16 @@ public class MailSpec extends Specification {
 			Mail.PROPERTIES.get('mail.smtp.host') == 'smtp.domain.com'
 			Mail.PROPERTIES.get('mail.smtp.port') == '25'
 			Mail.PROPERTIES.get('mail.transport.protocol') == 'smtp'
+	}
+	
+	def "Should be able to create a Mail Session"() {
+		setup:
+			def seesionMock = GroovyMock(Session, global: true)
+			def propsMock = Mock(Properties)
+		when:
+			def result = mail.getSession(propsMock)
+		then:
+			1 * Session.getDefaultInstance(propsMock) >> seesionMock
 	}
 
 }
